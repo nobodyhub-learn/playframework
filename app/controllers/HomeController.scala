e75f1d6fe910d480f517afe778564e06d17cbda0
javis-yan <yan_h@hotmail.com>
2018-08-06 09:25:01 +0800

    [Play!Framework]add nessessaries for a Play framework

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+/**
+  * @author yan_h
+  * @since 2018/8/6
+  */
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+    def index = Action {
+      Ok(views.html.index("Your new Application is ready!"))
+    }
+}
--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "MyPlayFramework"
 version := "0.1"
 
 scalaVersion := "2.12.6"
+
+lazy val `MyPlayFramework` = project.in(file(".")).enablePlugins(PlayScala)
+
+libraryDependencies += guice
